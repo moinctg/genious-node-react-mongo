f45e2959f365b994b93c65462f15d0b3e3ae68cf
Moinuddin Kamal <IT@pronayonbd.com>
2021-10-27 18:02:03 +0600

    project setup

---
+++ Genius-Car-Server/index.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const port = 5000;
+
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.send("hello world")
+})
+
+app.listen(port,()=>{
+    console.log("this port is ",port);
+})
